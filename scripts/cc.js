f48f162e9f35f66df1b1fbb50ee66e2afd39ca34
Jamie Skinner <skinner520@gmail.com>
2020-03-19 15:58:48 -0500

    feat: added a make things weird button

--- scripts/cc.js
+++ scripts/cc.js
@@ -1,3 +1,33 @@
+const wrapper = document.getElementById('wrapper')
+const crazyModeCheckbox = document.getElementById('crazy-mode')
+let timeoutId = ''
+
+crazyModeCheckbox.addEventListener('change',toggleCrazy)
+
+const gifLinks = [
+  'https://www.cameronsworld.net/img/content/2/28.gif',
+  'https://www.cameronsworld.net/img/content/1/16.gif',
+  'https://www.cameronsworld.net/img/content/2/2.gif',
+  'https://www.cameronsworld.net/img/content/2/7.gif',
+  'https://www.cameronsworld.net/img/content/5/3.gif',
+  'https://www.cameronsworld.net/img/content/2/9.gif',
+  'https://www.cameronsworld.net/img/content/1/7.gif',
+]
+
+let gifEls = []
+
+function toggleCrazy(e){
+  let isChecked = e.target.checked
+ if(isChecked){
+   postGifs()
+ } else {
+   window.clearTimeout(timeoutId)
+  document.body.style.backgroundColor = 'white'
+  gifEls.forEach(el => wrapper.removeChild(el))
+  gifEls = []
+ }
+}
+
 cheatCode(['ArrowUp',
           'ArrowUp',
           'ArrowDown',
@@ -7,16 +37,36 @@ cheatCode(['ArrowUp',
           'ArrowLeft',
           'ArrowRight',
           'b',
-          'a'], draw, 3000, correct, incorrect)
+          'a'], postGifs, 3000, correct, incorrect)
 
 var fps = 1;
 var animation;
 //callback function when code is complete
-function draw() {
-    setTimeout(function() {
+
+function postGifs(){
+  let wrapper = document.getElementById('wrapper')
+
+  gifLinks.forEach(link => {
+    let img = document.createElement('img')
+    img.setAttribute('src', link)
+    img.style.position = "absolute"
+    img.style.top = `${Math.random() * window.innerHeight}px`
+    img.style.left = `${Math.random() * window.innerWidth}px`
+    gifEls.push(img)
+    wrapper.appendChild(img)
+  })
+
+  function draw() {
+      timeoutId = setTimeout(function() {
         animation = requestAnimationFrame(draw);
         document.body.style.backgroundColor = 'hsl('+Math.floor(Math.random()*255)+', 50%, 50%)'
+        gifEls.forEach(el => {
+          el.style.top = `${Math.random() * window.innerHeight}px`
+          el.style.left = `${Math.random() * window.innerWidth}px`
+        })
     }, 1000 / fps);
+  }
+  draw()
 }
 
 var lis = Array.prototype.slice.call(document.querySelectorAll('li'))
