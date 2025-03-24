1004da97dd36e9925b3fa577008939a472510ce0
Jamie Skinner <jamie@etcetera.cr>
2025-03-24 11:22:51 -0500

    tweaks

--- src/pages/FAQ.js
+++ src/pages/FAQ.js
@@ -1,4 +1,3 @@
-import { Link } from '@reach/router'
 import React, { useContext } from 'react'
 import { StoreContext } from '../store'
 
@@ -19,22 +18,16 @@ export function FAQ(props) {
         Grande.
       </p>
 
-      <h4>I'm a recruiter for a very serious role. Why is this site kinda trash? Is this a joke to you?</h4>
-      <p>
-        Yeah, kinda. Listen, I'm very serious about doing good work. But not all good work has to be so serious. I think we can both tell this site is largely a joke. And I could build it a whole lot better. But frankly I really don't want to spend any more time on self promotion than is absolutely necessary. I'm sure you're probably bored at looking at stale resumes and cover letters all day. So why don't you sit back, relax. Play some snake. You can tell your boss you're doing research.
-      </p>
-      <p>But if you don't make it past 25, you have to hire me. Deal?</p>
-
       <h4>Something is broken. Why don't you fix it</h4>
       <p>
         I originally built like 6 years ago, and then refactored to be less
-        annoyingly minimalist 4 years ago... and you know what i dont need to
+        annoyingly minimalist 4 years ago... and you know what i don't need to
         explain myself. I'll get to it eventually.
       </p>
 
       <h4>What technologies are you familiar with?</h4>
       <p>
-        ah geez... too many to list. I mostly write in Typescript, but im not too bad at Python. Here are some technologies you might be interested in: React, NextJS, React Native, GraphQL, Docker, BullMQ, Redis, Postgres, MongoDB, Stripe, d3, ThreeJS... I can keep going.
+        ah geez... too many to list. I mostly write in Typescript, but im not too bad at Python. Here are some technologies you might be interested in: React, NextJS, React Native, GraphQL, Figma, Miro, Jira, Photoshop, Illustrator, Docker, BullMQ, Redis, Postgres, MongoDB, Stripe, d3, ThreeJS... I can keep going.
       </p>
 
       <h4>Are you available to work on my project?</h4>
@@ -46,23 +39,21 @@ export function FAQ(props) {
 
       <h4>What sort of art do you do?</h4>
       <p>
-        Mostly mural arts. But I am in the process of combining my propensity
-        for technology with my love of visual arts. More to come! If you want to
-        see an Augmented Reality Project I did, watch{' '}
+        Mostly mural arts. Or rather big pieces with spray paint. But I am in the process of combining my propensity for technology with my love of visual arts. More to come! If you want to see an Augmented Reality Project I did, watch{' '}
         <a href="https://www.youtube.com/watch?v=GtpZDYSYy5s">
           this short documentary about it
         </a>
-        . It's goofy.
+        . It's goofy and kinda makes no sense.
       </p>
 
       <h4>What the hell is a chalupa?</h4>
       <p>
-        Its like a fried taco I guess.
+        It's like a fried taco I guess.
       </p>
 
       <h4>What the hell is a chalupa grande?</h4>
       <p>
-        Its a big fried taco.
+        It's a big fried taco.
       </p>
 
       <h4>What is your hidden talent?</h4>
