464fd3699d8e82cfd4f58cfe9fb4f95efd2ed2c2
Jamie Skinner <jamie@etcetera.cr>
2025-04-21 17:47:01 -0500

    add next track

--- src/components/MusicPlayer/musicPlayer.js
+++ src/components/MusicPlayer/musicPlayer.js
@@ -1,4 +1,5 @@
 import React, { useRef, useState, useEffect } from "react"
+import tracks from "../../assets/tracks"
 import "./musicPlayer.css"
 
 
@@ -8,6 +9,7 @@ export function MusicPlayer() {
   const [seek, setSeek] = useState(0)
   const [curTime, setCurTime] = useState(0)
   const [duration, setDuration] = useState(0)
+  const [curTrackId, setCurTrackId] = useState(0)
 
   function handlePlayPause(e) {
     if (!isPaused) {
@@ -17,6 +19,19 @@ export function MusicPlayer() {
       player.current.pause()
     }
     setIsPaused(!isPaused)
+  }
+
+  function nextTrack() {
+    if (curTrackId < tracks.length - 1) {
+      setCurTrackId(curTrackId + 1)
+    } else {
+      setCurTrackId(0)
+    }
+
+    player.current.load()
+    setTimeout(() => {
+      handlePlayPause()
+    }, 100)
   }
 
   function handleSeek(e) {
@@ -51,7 +66,7 @@ export function MusicPlayer() {
         setSeek(seekValue)
         setCurTime(calculateTime(currentTimeInSecs))
         if (currentTimeInSecs === durationInSecs) {
-          handlePlayPause()
+          nextTrack()
         }
       })
 
@@ -63,31 +78,33 @@ export function MusicPlayer() {
 
   return (
     <div className="music-player-container">
-      <audio className="hidden" controls ref={player} loop>
+      <audio className="hidden" controls ref={player}>
         <source
-          src="https://chalupagrande.nyc3.cdn.digitaloceanspaces.com/portfolio/audio/neon-echos-2.mp3"
+          src={tracks[curTrackId].source}
           type="audio/mpeg"
         />
         Your browser does not support the audio element.
       </audio>
 
       <div className="music-player">
-        <div className="track-">
-          <div className="nowplaying">
-            <div className="cover">
-              <img width="60" src="https://chalupagrande.nyc3.cdn.digitaloceanspaces.com/portfolio/images/neon-echos-cover.png" alt="Album Cover" />
-            </div>
-            <div className="track-info">
-              <p className="track-title">Neon Chalupa</p>
-              <p className="artist">by chalupagrande</p>
-            </div>
+        <div className="track-details">
+          <div className="cover">
+            <img width="60" src={tracks[curTrackId].cover} alt="Album Cover" />
           </div>
-          <div className="controls">
-            <span className={`playpause${isPaused ? " paused" : ""}`} onClick={handlePlayPause} />
-            <span className="curTime">{curTime}</span>
-            <input type="range" id="seek-slider" max="100" value={seek} onChange={handleSeek} />
-            <span className="duration">{duration}</span>
+          <div className="track-info">
+            <span className="now-playing">now playing:</span>
+            <p className="track-title">{tracks[curTrackId].name}</p>
+            <p className="artist">by {tracks[curTrackId].artist}</p>
           </div>
+        </div>
+        <div className="controls">
+          <div className="control-btns">
+            <span className={`ctl-btn playpause${isPaused ? " paused" : ""}`} onClick={handlePlayPause} />
+            <span className="ctl-btn next" onClick={nextTrack} />
+          </div>
+          <span className="curTime">{curTime}</span>
+          <input type="range" id="seek-slider" max="100" value={seek} onChange={handleSeek} />
+          <span className="duration">{duration}</span>
         </div>
       </div>
     </div>
